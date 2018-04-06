4215dc998988ac34daee224a243708bf75308c9e
ColorPalmy <32357751+ColorPalmy@users.noreply.github.com>
2018-04-06 10:31:09 +0200

    Implementation Dijkstra & Label

--- src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
+++ src/main/org/insa/algo/shortestpath/DijkstraAlgorithm.java
@@ -1,5 +1,16 @@
 package org.insa.algo.shortestpath;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.insa.algo.AbstractSolution.Status;
+import org.insa.algo.utils.BinaryHeap;
+import org.insa.graph.Arc;
+import org.insa.graph.Graph;
+import org.insa.graph.Node;
+import org.insa.graph.Path;
+
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     public DijkstraAlgorithm(ShortestPathData data) {
@@ -8,9 +19,96 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
     @Override
     protected ShortestPathSolution doRun() {
-        ShortestPathData data = getInputData();
+    	
+    	// Retrieve the graph.
+    	ShortestPathData data = getInputData();
+    	Graph graph = data.getGraph();
+    	
+		final int nbNodes = graph.size();
+		
+		// Initialize array of labels.
+		Label[] marks = new Label[nbNodes];
+		for (Node node: graph) {
+			marks[node.getId()]= new Label(node);
+		}
+		marks[data.getOrigin().getId()].setCout(0.0);
+		
+		// Initialize array of predecessors.
+		Arc[] predecessorArcs = new Arc[nbNodes];
+		
+		//Creation of the BinaryHeap of Labels.
+		BinaryHeap<Label> tas = new BinaryHeap<Label>();
+		
+		// Notify observers about the first event (origin processed).
+		notifyOriginProcessed(data.getOrigin());
+		
+		//Insertion du sommet initial dans le tas
+		tas.insert(marks[data.getOrigin().getId()]);
+        
+        boolean stillExistNotMarked = true;
+        Node x;
+        Node y;
+        Label lx;
+        Label ly;
+        double oldCost, newCost;
+        
+        while(stillExistNotMarked) {
+        	
+        	stillExistNotMarked = false;
+        	
+        	lx = tas.findMin();
+        	x = tas.findMin().getNode();
+        	
+        	lx.setMarked(true);
+        	System.out.println("node markee " + x.getId());
+        	
+        	for(Arc arc: x) {
+        		
+        		ly = marks[arc.getDestination().getId()];
+        		y = marks[arc.getDestination().getId()].getNode();
+        		
+        		if (!ly.isMarked()) {
+        			
+        			stillExistNotMarked = true;
+        			
+        			oldCost = ly.getCout();
+        			newCost = Math.min(oldCost, lx.getCout()+data.getCost(arc));
+        			ly.setCout(newCost);
+        			
+        			if (newCost != oldCost) {
+        				tas.insert(ly);
+        				predecessorArcs[arc.getDestination().getId()] = arc;
+        			}
+        		}
+        	}
+        }
+        System.out.println("sorti du while");
+        
         ShortestPathSolution solution = null;
-        // TODO:
+        
+     // Destination has no predecessor, the solution is infeasible...
+     		if (predecessorArcs[data.getDestination().getId()] == null) {
+     			solution = new ShortestPathSolution(data, Status.INFEASIBLE);
+     		} else {
+
+     			// The destination has been found, notify the observers.
+     			notifyDestinationReached(data.getDestination());
+
+     			// Create the path from the array of predecessors...
+     			ArrayList<Arc> arcs = new ArrayList<>();
+     			Arc arc = predecessorArcs[data.getDestination().getId()];
+     			while (arc != null) {
+     				arcs.add(arc);
+     				arc = predecessorArcs[arc.getOrigin().getId()];
+     			}
+
+     			// Reverse the path...
+     			Collections.reverse(arcs);
+
+     			// Create the final solution.
+     			solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, arcs));
+     		}
+
         return solution;
     }
 
---
+++ src/main/org/insa/algo/shortestpath/Label.java
@@ -0,0 +1,53 @@
+package org.insa.algo.shortestpath;
+
+import org.insa.graph.Node;
+
+public class Label implements Comparable<Label>{
+
+	private boolean marked;
+	private double cout;
+	private Node node;
+	
+	public Label(Node node) {
+		this.marked = false;
+		this.node = node;
+		this.cout = Double.POSITIVE_INFINITY;
+	}
+	
+	public Label(Node node, boolean mark, double cout) {
+		this.marked = mark;
+		this.cout = cout;
+		this.node = node;
+	}	
+
+	public boolean isMarked() {
+		return this.marked;
+	}
+	
+	public void setMarked(boolean value) {
+		this.marked = value;
+	}
+	
+	public void setCout(double cout) {
+		this.cout = cout;
+	}
+
+	public Node getNode() {
+		return node;
+	}
+
+	public void setNode(Node node) {
+		this.node = node;
+	}
+
+	public double getCout() {
+		return cout;
+	}
+
+	@Override
+	public int compareTo(Label o) {
+		return Double.compare(this.cout,  o.cout);
+	}
+
+
+}
