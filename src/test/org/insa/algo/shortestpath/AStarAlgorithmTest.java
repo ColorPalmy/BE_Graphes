95f65beb6fb80f44fcd9e18467798afec6fc99a0
ColorPalmy <32357751+ColorPalmy@users.noreply.github.com>
2018-06-02 15:19:20 +0200

    test is working

--- src/test/org/insa/algo/shortestpath/AStarAlgorithmTest.java
+++ src/test/org/insa/algo/shortestpath/AStarAlgorithmTest.java
@@ -20,117 +20,62 @@ public class AStarAlgorithmTest extends DijkstraAlgorithmTest{
         // Create nodes
         nodes = new Node[6];
 
-        Point[] points = new Point[6];
-        points[0] = new Point(0, 0.5f);
-        points[1] = new Point(0.5f, 1);
-        points[2] = new Point(0.5f, 0);
-        points[3] = new Point(1, 1.5f);
-        points[4] = new Point(1.5f, 1);
-        points[5] = new Point(1.5f, 0);
-        nodes[0] = new Node(0, points[0]);
-        nodes[1] = new Node(1, points[1]);
-        nodes[2] = new Node(2, points[2]);
-        nodes[3] = new Node(3, points[3]);
-        nodes[4] = new Node(4, points[4]);
-        nodes[5] = new Node(5, points[5]);
+        Point point = new Point(0,0);
+
+        
+        for (int i = 0; i < nodes.length; ++i) {
+            nodes[i] = new Node(i, point);
+        }
 
         ArrayList<Point> liste_points = new ArrayList<Point>();
         
-        liste_points.add(points[0]);
-        liste_points.add(points[1]);
         Node.linkNodes(nodes[0], nodes[1], 7,
                 new RoadInformation(RoadType.UNCLASSIFIED, null, true, 1, null),
                 liste_points);
         
-        liste_points.clear();
-        liste_points.add(points[0]);
-        liste_points.add(points[2]);
-        
         Node.linkNodes(nodes[0], nodes[2], 8,
                 new RoadInformation(RoadType.UNCLASSIFIED, null, true, 1, null),
                 liste_points);
-        
-        liste_points.clear();
-        liste_points.add(points[1]);
-        liste_points.add(points[3]);
-        
+
         Node.linkNodes(nodes[1], nodes[3], 4,
                 new RoadInformation(RoadType.UNCLASSIFIED, null, true, 1, null),
                 liste_points);
-        
-        liste_points.clear();
-        liste_points.add(points[1]);
-        liste_points.add(points[4]);
         
         Node.linkNodes(nodes[1], nodes[4], 1,
                 new RoadInformation(RoadType.UNCLASSIFIED, null, true, 1, null),
                 liste_points);
         
-        liste_points.clear();
-        liste_points.add(points[1]);
-        liste_points.add(points[5]);
-        
         Node.linkNodes(nodes[1], nodes[5], 5,
                 new RoadInformation(RoadType.UNCLASSIFIED, null, true, 1, null),
                 liste_points);
-        
-        liste_points.clear();
-        liste_points.add(points[2]);
-        liste_points.add(points[0]);
         
         Node.linkNodes(nodes[2], nodes[0], 7,
                 new RoadInformation(RoadType.UNCLASSIFIED, null, true, 1, null),
                 liste_points);
         
-        liste_points.clear();
-        liste_points.add(points[2]);
-        liste_points.add(points[1]);
-        
         Node.linkNodes(nodes[2], nodes[1], 2,
                 new RoadInformation(RoadType.UNCLASSIFIED, null, true, 1, null),
                 liste_points);
         
-        liste_points.clear();
-        liste_points.add(points[2]);
-        liste_points.add(points[5]);
-        
         Node.linkNodes(nodes[2], nodes[5], 2,
                 new RoadInformation(RoadType.UNCLASSIFIED, null, true, 1, null),
                 liste_points);
-        
-        liste_points.clear();
-        liste_points.add(points[4]);
-        liste_points.add(points[2]);
-        
+
         Node.linkNodes(nodes[4], nodes[2], 2,
                 new RoadInformation(RoadType.UNCLASSIFIED, null, true, 1, null),
                 liste_points);
-        
-        liste_points.clear();
-        liste_points.add(points[4]);
-        liste_points.add(points[3]);
         
         Node.linkNodes(nodes[4], nodes[3], 2,
                 new RoadInformation(RoadType.UNCLASSIFIED, null, true, 1, null),
                 liste_points);
         
-        liste_points.clear();
-        liste_points.add(points[4]);
-        liste_points.add(points[5]);
-        
         Node.linkNodes(nodes[4], nodes[5], 3,
                 new RoadInformation(RoadType.UNCLASSIFIED, null, true, 1, null),
                 liste_points);
         
-        liste_points.clear();
-        liste_points.add(points[5]);
-        liste_points.add(points[4]);
-        
         Node.linkNodes(nodes[5], nodes[4], 3,
                 new RoadInformation(RoadType.UNCLASSIFIED, null, true, 1, null),
                 liste_points);
-        
-        liste_points.clear();
 
         
         graph = new Graph("ID", "", Arrays.asList(nodes), null);
